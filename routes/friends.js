9a4d5756a9799567971622c7e6a7ec3a8205b40c
Marisa Kanemoto <marisa.kanemoto@gmail.com>
2016-02-24 04:34:36 +0000

    made friends.handlebars dynamic, also added a user called bagels

--- routes/friends.js
+++ routes/friends.js
@@ -7,25 +7,28 @@ var usersArr = users["users"];
 var friends = usersArr[0].friends;  
 
 exports.view = function(req, res) {
-	res.render('friends', friends);
+	var usersFriends = [];
+	for (i = 0; i < friends.length; i++) {
+		console.log("friends: ")
+		console.log(friends[i]);
+		var thisFriend = findUserByUsername(friends[i].username);
+		usersFriends.push(thisFriend);
+	}
+	console.log("usersFriends: ")
+	console.log(usersFriends);
+	res.render('friends', {'usersFriends':usersFriends});
 }
 
 /* helper function to find users given only their username */
-function findUserByUsername(username){
+function findUserByUsername(uname){
 	var friends = friends;
-	var uname = username;
-	for (i = 0; i < users.length; i++) {
-		if (friends[i].hasOwnProperty("username") && friends[i]["username"] === uname) {
-			var thisFriend = friends[i];
+	var uname = uname;
+	for (i = 0; i < usersArr.length; i++) {
+		if (usersArr[i].hasOwnProperty("username") && usersArr[i]["username"] === uname) {
+			var thisFriend = usersArr[i];
+			console.log("thisFriend: ")
+			console.log(thisFriend);
 			return thisFriend;
 		}
 	}
-}
-
-/* function to return information for each friend */
-function friendInfo(username) {
-	var thisFriend = findUserByUsername(username);
-	var username = thisFriend.username;
-	var fullName = thisFriend.fullName;
-	var profPic = thisFriend.profPic;
 }
