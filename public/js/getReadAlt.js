ad78c7ddd855fa394a9ef504005dc7625b3f0b01
Raquel <calderon.rocky@gmail.com>
2016-03-02 02:28:35 +0000

    alt nav and alt getRead.js

---
+++ public/js/getReadAlt.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$('#likeBtn').click(likeBook);
+	$('#recBtn').click(function(){
+		$("#recommendFriendModal").modal();
+	})
+	$('#recommendToFriendSubmit').click(recBookFriend);
+}
+
+function likeBook(e) {
+	e.preventDefault();
+
+	var bookTitle = $('.title').text();
+	var url = '/like/' + bookTitle;
+	$.get(url, function(result) {
+		$('#likeBtn').blur();
+		if(result["liked"]) {
+			$('#likeBtn').css({
+				'background': 'url("../images/heart2.png") center no-repeat',
+				'background-size':' 46.2px 42px',
+				'background-color': '#fff'
+			});
+		}
+		else {
+			$('#likeBtn').css({
+				'background': 'url("../images/heart.png") center no-repeat',
+				'background-size':' 46.2px 42px',
+				'background-color': '#fff'
+			});
+		}
+	})
+}
+
+function recBookFriend(e) {
+	e.preventDefault();
+	
+	var bookTitle = $('.title').text();
+	var friend = $('#selFriend').val();
+
+	var url = '/recommend/' + bookTitle + '/' + friend;
+	$.get(url, function(result) {
+		$('#recBtn').blur();
+		if(result["recommended"]) {
+			$('#recBtn').css({
+				
+				'color': 'white'
+			});
+		}
+		else {
+			$('#recBtn').css({
+				'background-color': '#fff',
+				'color': '#333'
+			});
+		}
+	})
+}
